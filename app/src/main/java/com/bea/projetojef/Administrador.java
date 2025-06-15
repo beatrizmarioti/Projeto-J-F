04e14395fdec4265923e3e75d6fc4621ab5d009d
Lorenaatavares <157626068+Lorenaatavares@users.noreply.github.com>
2025-06-15 13:15:46 -0300

    terminandoo

--- app/src/main/java/com/bea/projetojef/Administrador.java
+++ app/src/main/java/com/bea/projetojef/Administrador.java
@@ -1,29 +1,29 @@
-package com.bea.projetojef;
+    package com.bea.projetojef;
 
-public class Administrador {
-    private String cracha;
-    private String nome;
-    private String inicio;
-    private long id;
+    public class Administrador {
+        private String cracha;
+        private String nome;
+        private String inicio;
+        private long id;
 
-    public Administrador() {}
+        public Administrador() {}
 
-    public Administrador(long id, String cracha, String nome, String inicio) {
-        this.id = id;
-        this.cracha = cracha;
-        this.nome = nome;
-        this.inicio = inicio;
+        public Administrador(long id, String cracha, String nome, String inicio) {
+            this.id = id;
+            this.cracha = cracha;
+            this.nome = nome;
+            this.inicio = inicio;
+        }
+
+        // Getters
+        public long getId() { return id; }
+        public String getCracha() { return cracha; }
+        public String getNome() { return nome; }
+        public String getInicio() { return inicio; }
+
+        // Setters
+        public void setId(long id) { this.id = id; }
+        public void setCracha(String cracha) { this.cracha = cracha; }
+        public void setNome(String nome) { this.nome = nome; }
+        public void setInicio(String inicio) { this.inicio = inicio; }
     }
-
-    // Getters
-    public long getId() { return id; }
-    public String getCracha() { return cracha; }
-    public String getNome() { return nome; }
-    public String getInicio() { return inicio; }
-
-    // Setters
-    public void setId(long id) { this.id = id; }
-    public void setCracha(String cracha) { this.cracha = cracha; }
-    public void setNome(String nome) { this.nome = nome; }
-    public void setInicio(String inicio) { this.inicio = inicio; }
-}
